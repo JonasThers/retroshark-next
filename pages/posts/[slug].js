d4c7239bb80a5a8cce3ff1aab8fe944b68c9dc46
JonasThers <jonasthers1992@gmail.com>
2021-11-09 16:04:47 +0200

    Reset pre-rendering issues

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import PostActions from "../../components/PostActions";
 
 export default function Post({ post }) {
-  console.log("https://calm-ocean-22227.herokuapp.com" + `${post.Image.url}`)
   return (
     <div>
       <Head>
@@ -14,7 +13,7 @@ export default function Post({ post }) {
         {post && post.Image ? (
           <img
             className="content__image"
-            src={"https://calm-ocean-22227.herokuapp.com" + `${post.Image.url}`}
+            src={"https://calm-ocean-22227.herokuapp.com" + `${post ? post.Image.url : '' }`}
             alt="Segun Adebayo"
           />
         ) : (
